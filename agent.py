e456ab4a0921200a9ae0f79c34fd94739b271eaa
shresth goel <shresthgoel77@gmail.com>
2026-05-20 23:30:47 +0530

    Add WeatherRetrievalAgent class for weather API

--- agent.py
+++ agent.py
@@ -1,1 +1,20 @@
+import requests
+
+class WeatherRetrievalAgent:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    def get_weather(self, city):
+        params = {"q": city, "appid": self.api_key, "units": "metric"}
+        res = requests.get(self.base_url, params=params)
+        if res.status_code == 200:
+            data = res.json()
+            return {
+                "city": city,
+                "temperature": data["main"]["temp"],
+                "humidity": data["main"]["humidity"],
+                "condition": data["weather"][0]["description"]
+            }
+        return {"error": "City not found"}
 
